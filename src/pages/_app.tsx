a2ea6faf4c0fee3dc71839d10d66e9f3422df410
thmata <taiagomata@gmail.com>
2022-06-23 23:46:41 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/global.scss'
 import type { AppProps } from 'next/app'
+import styles from '../styles/app.module.scss'
+import { Header } from '../components/Header/Header'
+import { Player } from '../components/Player/Player'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <div className={styles.wrapper}>
+    <main>
+      <Header/>
+      <Component {...pageProps} />
+    </main>
+    <Player/>
+  </div>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,82 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react';
+import { GetStaticProps } from 'next'
+import { api } from '../services/api';
 
-const Home: NextPage = () => {
+/* // SPA
+    useEffect(() => {
+        fetch('http://localhost:3333/episodes')
+          .then(response => response.json())
+          .then(data => console.log(data))
+      }, [])
+*/
+
+// SSR -> Vai  executar toda vez que acessar a home, se ela não sofre muitas alterações não tem porque ele toda vez ir na API buscar.
+
+      // export async function getServerSideProps() {
+      //   const response = await fetch('http://localhost:3333/episodes')
+      //   const data = await response.json()
+
+      //   return {
+      //     props:{
+      //       episodes: data,
+      //     }
+      //   } 
+
+//      }
+
+
+// SSG -> Vai ser usado para formas estáticas, já que não vai ter muitas mudanças/ muitas requisições.
+
+
+      // export async function getStaticProps() {
+      //   const response = await fetch('http://localhost:3333/episodes')
+      //   const data = await response.json()
+
+      //   return {
+      //     props:{
+      //       episodes: data,
+      //     },
+      //    revalidate: 60 * 60 * 8 // 60 segundos x 60 segundos * 8 = 8 horas
+      //   } 
+
+      // }
+
+type HomeProps ={
+  episodes: Array<{
+    id: string
+    title: string
+    members: string
+    published_at: string
+    thumbnail: string
+    description: string
+  }> 
+}
+
+export default function Home(props: HomeProps){
   return (
     <div>
-      <h1>Olá</h1>
+      <p>Indesx</p>
     </div>
   );
 }
 
-export default Home
+
+export const getStaticProps: GetStaticProps  = async () =>  {
+    const { data } = await api.get('episodes', {
+      params: {
+        _limit: 12,
+        _sort: 'published_at',
+        _order: 'desc'
+      }
+    })
+
+    return {
+      props:{
+        episodes: data,
+      },
+      revalidate: 60 * 60 * 8 // 60 segun dos x 60 segundos * 8 = 8 horas
+    }
+    
+
+}
